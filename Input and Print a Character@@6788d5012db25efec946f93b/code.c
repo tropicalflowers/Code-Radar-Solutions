d43496842bf6783bacc7774fffaf9a60aa91a622
tropicalflowers <tanseemkashyap@gmail.com>
2025-01-29 10:28:34 +0530

    Solution to Input and Print a Character

---
+++ Input and Print a Character@@6788d5012db25efec946f93b/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    printf("%s", welcome());
+    char str[100];
+    scanf("%s", &str);
+    printf("%s", str)
+    return 0;
+}
