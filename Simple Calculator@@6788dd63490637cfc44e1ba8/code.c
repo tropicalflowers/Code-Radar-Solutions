a664fbb5278c672f5eebd2f3f026ca864588790c
tropicalflowers <tanseemkashyap@gmail.com>
2025-02-04 11:15:58 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,15 +4,35 @@ int main()
     int a,b;
     char c;
     int sum;
-    
+    int minus;
+    int multiply;
+    int divide;
   
     scanf("%d %d %c", &a, &b, &c);
    
     sum=a+b;
+    minus=a-b;
+    multiply=a*b;
+    divide=a/b;
+
     switch(c)
     {
         case '+':
         printf("%d", sum);
         break;
+        case '-':
+        printf("%d", minus);
+        break;
+        case '*':
+        printf("%d", multiply);
+        break;
+        case '/':
+
+        printf("%d", divide);
+        break;
+        default:
+        printf("error");
+
     }
+    return 0;
 }
