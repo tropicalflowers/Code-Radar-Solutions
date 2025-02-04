e3b6893595cddde9e9ad9b338205be4da21ce734
tropicalflowers <tanseemkashyap@gmail.com>
2025-02-04 11:30:32 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    char a;
+    scanf("%c", &a);
+    switch(a)
+    {
+        case 'A':
+        printf("Excellent");
+        break;
+        case 'B':
+        printf("Good");
+        break;
+        case 'C':
+        printf("Average");
+        break;
+        case 'D':
+        case 'F':
+
+    }
+   return 0;
+}
