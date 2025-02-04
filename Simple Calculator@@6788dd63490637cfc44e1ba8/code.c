245c529932be34d03b2bb38f43e09350a4abed9e
tropicalflowers <tanseemkashyap@gmail.com>
2025-02-04 11:01:48 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    char c;
+    int sum=a+b;
+    scanf("%d %d %c", &a,&b,&c);
+    switch(c)
+    {
+        case '+':
+        printf("%d", sum)
+        break;
+    }
+}
