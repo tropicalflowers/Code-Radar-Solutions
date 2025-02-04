96ef96e8283949b42d81b9f20c338ed2569fad32
tropicalflowers <tanseemkashyap@gmail.com>
2025-02-04 11:38:32 +0530

    Solution to Traffic Light Simulation

--- Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -10,13 +10,6 @@ int main() {
         case 'G':
         printf("Go");
         break;
-        case 'Y':
-        printf("Slow Down");
-        break;
-        default:
-        printf("Invalid input");
-
     }
-
     return 0;
 }
