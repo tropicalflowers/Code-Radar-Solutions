cddf83c7c9bf774417292fdfa7086d386304dffd
tropicalflowers <tanseemkashyap@gmail.com>
2025-02-11 11:16:21 +0530

    Solution to Print a Square Pattern

--- Print a Square Pattern@@67877e1818d53eb724f435be/code.c
+++ Print a Square Pattern@@67877e1818d53eb724f435be/code.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+
 int main() {
-    int a,i,j;
-    scanf("%d"; &a);
-    for(i=1; i<=a; i++)
-    {
-        for(j=a; j<=a; j)
-        {printf("*");}
+    int a, i, j, k;
+    scanf("%d", &a);
+    for (i = 1; i <= a; i++) {
+        // Print spaces to center-align the pyramid
+        for (j = i; j < a; j++) {
+            printf(" ");
+        }
+        // Print stars to form the pyramid
+        for (k = 1; k <= (2 * i - 1); k++) {
+            printf("*");
+        }
+        // Move to the next line after each row
         printf("\n");
     }
     return 0;
